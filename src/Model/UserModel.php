d34c5a6da7c1d8be1dd4a6b6c7612dd8b603f727
Arturs Mednis <artursmednis2003@gmail.com>
2024-04-03 09:49:48 +0200

    avancement

---
+++ src/Model/UserModel.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Model;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+class UserModel
+{
+    private ?string $email = null;
+    private ?string $password = null;
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
