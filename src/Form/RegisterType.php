300b68d1ccfeb1e7e5758a157dc21e305c43e63c
arturs <artursmednis2003@gmail.com>
2024-06-03 14:19:26 +0200

    affinage

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -6,6 +6,7 @@ use App\Model\UserModel;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -18,9 +19,15 @@ class RegisterType extends AbstractType
             ->add('email',TextType::class,[
                 'label'=>"Mail"
             ])
-            ->add('password',PasswordType::class,[
-                'label'=>"Mot de passe"
+            ->add('password',RepeatedType::class,[
+                'type'=>PasswordType::class,
+                'invalid_message'=>"Les mots de passe ne correspondent pas",
+                'label'=>"Mot de passe",
+                'required'=>true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmation Mot de passe'],
             ])
+
         ;
     }
 
